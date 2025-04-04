6f66568c0e68c63b27ec3b2932cfb745fe261373
Diego_Alvarez <diego_alvarez@fragua.com.mx>
2025-04-04 12:42:13 -0600

    Changed page title and icon and added general styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import MergePage from './components/MergePage';
 
 function App() {
   return (
-    <>
+    <div className="h-full bg-[#f8f8f8]">
       <MergePage />
-    </>
+    </div>
   );
 }
 
