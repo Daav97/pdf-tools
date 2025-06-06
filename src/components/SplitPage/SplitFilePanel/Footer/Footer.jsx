edc35e16fcf77b755c840d084f55d4ea12b7c5c3
Diego_Alvarez <diego_alvarez@fragua.com.mx>
2025-06-06 10:13:28 -0600

    Added informative Modal about page selection

--- src/components/SplitPage/SplitFilePanel/Footer/Footer.jsx
+++ src/components/SplitPage/SplitFilePanel/Footer/Footer.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react';
 import CrossIcon from '../../../svg/CrossIcon';
+import Modal from '../../../Modal';
 
 const Footer = () => {
   const [newSplitValue, setNewSplitValue] = useState('');
+  const [showSplitInfoHelp, setShowSplitInfoHelp] = useState(false);
 
   const handleSplitInput = (event) => {
     setNewSplitValue(event.target.value);
@@ -15,7 +17,10 @@ const Footer = () => {
       </span>
       <div className="flex flex-1 items-center gap-2">
         <div className="relative flex-1 bg-emerald-200">
-          <span className="absolute top-1/2 -left-6 flex h-5 w-5 -translate-y-1/2 cursor-pointer items-center justify-center rounded-full border border-neutral-400 bg-neutral-100 text-neutral-600 hover:scale-110">
+          <span
+            className="absolute top-1/2 -left-6 flex h-5 w-5 -translate-y-1/2 cursor-pointer items-center justify-center rounded-full border border-neutral-400 bg-neutral-100 text-neutral-600 select-none hover:scale-110"
+            onClick={() => setShowSplitInfoHelp(true)}
+          >
             i
           </span>
           <input
@@ -37,6 +42,36 @@ const Footer = () => {
           <CrossIcon className="rotate-45" />
         </button>
       </div>
+      {showSplitInfoHelp && (
+        <Modal closeCallback={() => setShowSplitInfoHelp(false)}>
+          <div className="flex h-64 w-sm flex-col overflow-y-auto xl:w-md 2xl:w-xl">
+            <div className="flex w-full items-center justify-between">
+              <p className="font-bold">
+                Agrega páginas y/o rangos para la separación:
+              </p>
+              <button onClick={() => setShowSplitInfoHelp(false)}>
+                <CrossIcon className="h-6 w-6 cursor-pointer" />
+              </button>
+            </div>
+            <div className="mt-4">
+              <p className="mt-2">
+                Utiliza comas ( , ) para separar páginas individuales y/o
+                rangos.
+              </p>
+              <p>Utiliza guiones ( - ) para definir un rango de páginas.</p>
+              <p className="mt-6 mb-2 font-light underline">Ejemplos:</p>
+              <ul className="list-inside list-disc font-light">
+                <li>1, 3, 5 selecciona las páginas 1, 3 y 5. </li>
+                <li>2-4 selecciona las páginas 2, 3 y 4.</li>
+                <li>
+                  1, 4-6, 9 selecciona la página 1, las páginas 4 a 6 y la
+                  página 9.
+                </li>
+              </ul>
+            </div>
+          </div>
+        </Modal>
+      )}
     </div>
   );
 };
